6c49bcf710dffdb411e96d5c5192e4a7c108f3b8
tieuquang <tieuquang69@gmail.com>
2024-07-31 16:32:45 +0700

    add

--- config/cors.ts
+++ config/cors.ts
@@ -6,11 +6,20 @@ import { defineConfig } from '@adonisjs/cors'
  *
  * https://docs.adonisjs.com/guides/security/cors
  */
+// const corsConfig = defineConfig({
+//   enabled: true,
+//   origin: ['http://localhost:3000'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   headers: ['Content-Type', 'Accept', 'Authorization'],
+//   exposeHeaders: [],
+//   credentials: true,
+//   maxAge: 90,
+// })
 const corsConfig = defineConfig({
   enabled: true,
-  origin: ['http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  headers: ['Content-Type', 'Accept', 'Authorization'],
+  origin: true,
+  methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
+  headers: true,
   exposeHeaders: [],
   credentials: true,
   maxAge: 90,
